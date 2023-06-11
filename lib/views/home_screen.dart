8577125e6b628b84a194dad25bea2294e1c626fd
sohag <injamulhaqsohag19977@gmail.com>
2023-06-11 11:14:24 +0600

    store and print list data

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -23,9 +23,20 @@ class _HomeScreenState extends State<HomeScreen> {
           var box = await Hive.openBox("info");
           box.put("name", "Sohag");
           box.put("address", "Dhaka");
+          box.put("listData", [
+            {
+              'name': "Raiyan",
+              'age': 4,
+            },
+            {
+              'name': "Sharif",
+              'age': 24,
+            }
+          ]);
           print("=====print data=====");
           print(box.get("name"));
           print(box.get("address"));
+          print(box.get("listData")[1]['name']);
         },
         child: Text("Add"),
       ),
